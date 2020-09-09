28b61e8ad37b2322d7180f588c7ec1bf820f16e8
leobert-lan <774057695@qq.com>
2020-09-09 23:53:01 +0800

    feat:reset package name of user module

--- user/src/main/java/osp/leobert/android/github/user/login/LoginActivity.kt
+++ user/src/main/java/osp/leobert/android/github/user/login/LoginActivity.kt
@@ -1,11 +1,9 @@
 package osp.leobert.android.github.user.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import osp.leobert.magnetsample.foo.R
+import osp.leobert.android.github.user.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
